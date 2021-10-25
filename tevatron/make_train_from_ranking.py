001c11c014752b9847e0fcc9bab67c9815601d4b
Hang Li <cecillll.lee@gmail.com>
2021-10-25 13:14:06 +1000

    update make train

--- tevatron/make_train_from_ranking.py
+++ tevatron/make_train_from_ranking.py
@@ -69,6 +69,7 @@ if __name__ == '__main__':
     parser.add_argument('--collection-file', type=str, default='./data/msmarco_passage/collection/collection.tsv')
     parser.add_argument('--pair-file', type=str, default='./data/msmarco_passage/qrels/train_query_passage_pair.tsv')
     parser.add_argument('--encoder', type=str, default='./data/msmarco_passage/models/ANCE')
+    parser.add_argument('--model-type', type=str, default='ANCE', help='Can be ANCE, TCT, DistilBERT')
     parser.add_argument('--sample', type=int, default=100)
     parser.add_argument('--from-top', type=int, default=1000)
     parser.add_argument('--prf-k', type=int, default=3)
@@ -80,11 +81,11 @@ if __name__ == '__main__':
     collection = read_collection(args.collection_file)
     pair = read_query_target_pair(args.pair_file)
 
-    if args.encoder.rsplit('/', 1)[1] == 'ance':
+    if args.model_type == 'ANCE':
         tokenizer = RobertaTokenizer.from_pretrained(args.encoder)
-    elif args.encoder.rsplit('/', 1)[1] == 'distillbert-balanced':
+    elif args.model_type == 'DistilBERT':
         tokenizer = DistilBertTokenizer.from_pretrained(args.encoder)
-    elif args.encoder.rsplit('/', 1)[1] == 'tct-colbert-v2-hnp':
+    elif args.model_type == 'TCT':
         tokenizer = BertTokenizer.from_pretrained(args.encoder)
     else:
         raise NotImplementedError()
@@ -101,7 +102,10 @@ if __name__ == '__main__':
                 prf_passages.append(collection[pid])
 
             prf_passages_concatenation = f'{tokenizer.sep_token}'.join(prf_passages)
-            prf_query = f'{tokenizer.cls_token} [Q] {original_query}{tokenizer.sep_token}{prf_passages_concatenation}{tokenizer.mask_token * 512}'
+            if args.model_type == 'TCT':
+                prf_query = f'{tokenizer.cls_token} [Q] {original_query}{tokenizer.sep_token}{prf_passages_concatenation}{tokenizer.mask_token * 512}'
+            else:
+                prf_query = f'{original_query}{tokenizer.sep_token}{prf_passages_concatenation}'
         else:
             prf_query = original_query
 
@@ -112,10 +116,18 @@ if __name__ == '__main__':
             else:
                 sample = args.sample
             random_negatives = random.sample(rankings[qid], k=sample)
-            negatives = [f'{tokenizer.cls_token} [D] {collection[d]}' for d in random_negatives]
-            train_instance = {
-                'query': prf_query,
-                'positives': [f'{tokenizer.cls_token} [D] {positive_passage}'],
-                'negatives': negatives
-            }
+            if args.model_type == 'TCT':
+                negatives = [f'{tokenizer.cls_token} [D] {collection[d]}' for d in random_negatives]
+                train_instance = {
+                    'query': prf_query,
+                    'positives': [f'{tokenizer.cls_token} [D] {positive_passage}'],
+                    'negatives': negatives
+                }
+            else:
+                negatives = [collection[d] for d in random_negatives]
+                train_instance = {
+                    'query': prf_query,
+                    'positives': [positive_passage],
+                    'negatives': negatives
+                }
             fout.write(f'{json.dumps(train_instance)}\n')
